3acd9628ec33704a7de1de5aacbda7a332f341df
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-07-23 08:14:04 -0700

    Preparing for deploy

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const POKEDEX = require("./pokedex.json");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
 const validTypes = [
   "Bug",
@@ -26,13 +33,48 @@ const validTypes = [
   "Water"
 ];
 
-function handleGetTypes(req, res) {
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/types", function handleGetTypes(req, res) {
   res.json(validTypes);
-}
+});
 
-app.get("/types", handleGetTypes);
+app.get("/pokemon", function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-const PORT = 8000;
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+
+  res.json(response);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NOCE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
